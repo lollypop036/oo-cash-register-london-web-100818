513fcc4f5d40f8802531ad77efcedb9058839e8b
github username <slonanuga@gmail.com>
2018-10-10 19:01:16 +0100

    just started

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
 
+def initialize(*discount)
+
+    @new = new
+    
+    @discount = discount
+
+    @total = 0
+    
+end
+
+attr_accessor :new, :total
+
+def total
+@total
+end
+
+
+
+
+
+
+
+
+
+end
