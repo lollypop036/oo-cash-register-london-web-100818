45f72b8ea9c495f15ae8481118ff04f7d02ed5d0
github username <slonanuga@gmail.com>
2018-10-10 21:50:58 +0100

    finished

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,51 @@
+
+
 class CashRegister
-  attr_accessor :total, :discount, :item, :price
+  attr_accessor :total, :item, :price
+  attr_reader :discount
+ 
 
-  def initialize(*)
+  def initialize(discount=0)
     @total = 0
+    @discount = discount
+    @@basket = []
+    @@hash = {}
   end
 
-  def discount
-    @discount = 20
-  end
+  
 
   def total
-    @total
+    @total.to_f
+  end
+ 
+  def add_item(item, price, quantity=(1))
+   
+    
+      @total = self.total + price * quantity
+      quantity.times do @@basket << item end
+        @@hash[item] = (price * quantity)
+ 
   end
 
-  def add_item(item, price, quantity=(1))
-    @total = self.total + price * quantity
+  def apply_discount
+    if self.discount == 20
+      @total = @total - @total * discount.to_f / 100
+      "After the discount, the total comes to $800."
+    
+    else
+      "There is no discount to apply."
+      
+    end
+
+  end
+
+  def items
+    @@basket
+  end
+
+
+  def void_last_transaction
+    @total = @total - @@hash.values[-1]
   end
 
 end
